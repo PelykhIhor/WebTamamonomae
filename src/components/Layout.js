1406bce403446c2707a06874dd8b6941a277ad3a
PelykhIhor <159559700+PelykhIhor@users.noreply.github.com>
2024-07-21 16:25:14 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,31 @@
+"use client";
+
+import { useEffect } from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import "../app/globals.css";
 
 export default function Layout({ children }) {
-    return (
+  useEffect(() => {
+    const handleBlur = () => {
+      document.title = "ШВИДКО ПОВЕРНИСЬ!!!";
+    };
+
+    const handleFocus = () => {
+      document.title = "Your Original Title"; // Replace with your original title
+    };
+
+    window.addEventListener("blur", handleBlur);
+    window.addEventListener("focus", handleFocus);
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      window.removeEventListener("blur", handleBlur);
+      window.removeEventListener("focus", handleFocus);
+    };
+  }, []);
+
+  return (
+
         <main>
             <Navbar />
             {children}
