7521c1b775590ddcb9ee25508e0679f2c319e141
PelykhIhor <159559700+PelykhIhor@users.noreply.github.com>
2024-07-21 16:55:59 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,18 +7,35 @@ import "../app/globals.css";
 
 export default function Layout({ children }) {
   useEffect(() => {
+    const greetUserKey = "greetUserShown";
+    const sixHoursInMs = 6 * 60 * 60 * 1000;
+
+    const greetUser = () => {
+      const hours = new Date().getHours();
+      if ( 6 < hours < 12) return "Доброго ранку";
+      if ( 12 < hours < 18) return "Добрий день";
+      return "Добрий вечір";
+    };
+
+    const lastGreetedTime = localStorage.getItem(greetUserKey);
+    const now = Date.now();
+
+    if (!lastGreetedTime || (now - parseInt(lastGreetedTime)) > sixHoursInMs) {
+      alert(greetUser() + "! Ласкаво просимо до Tamamonomae!");
+      localStorage.setItem(greetUserKey, now.toString());
+    }
+
     const handleBlur = () => {
       document.title = "ШВИДКО ПОВЕРНИСЬ!!!";
     };
 
     const handleFocus = () => {
-      document.title = "Tamamonomae"; // Replace with your original title
+      document.title = "Tamamonomae";
     };
 
     window.addEventListener("blur", handleBlur);
     window.addEventListener("focus", handleFocus);
 
-    // Cleanup event listeners on component unmount
     return () => {
       window.removeEventListener("blur", handleBlur);
       window.removeEventListener("focus", handleFocus);
@@ -26,12 +43,11 @@ export default function Layout({ children }) {
   }, []);
 
   return (
-
-        <main>
-            <Navbar />
-            {children}
-            <Footer />
-            <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
-        </main>
-    );
+    <main>
+      <Navbar />
+      {children}
+      <Footer />
+      <script src="https://unpkg.com/flowbite@1.4.1/dist/flowbite.js"></script>
+    </main>
+  );
 }
